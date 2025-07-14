ae295851527860f28afd853fd9b0accc79b8de4c
wuyepabdul <wuyepabdul@gmail.com>
2025-07-14 22:48:37 +0100

    Add route to check if user is logged in

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/signup", signup);
@@ -9,5 +10,9 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.post("/onboarding", protectRoute, onboard);
+
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 
 export default router;
