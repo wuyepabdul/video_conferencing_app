c18d4c124cd94031991dbd499c5600351cab34e9
wuyepabdul <wuyepabdul@gmail.com>
2025-07-14 23:25:01 +0100

    Create getMyFriends controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -15,8 +15,22 @@ export const getRecommendedUsers = async (req, res) => {
     res.status(200).json(recommendedUsers);
   } catch (error) {
     console.log("Error in get recommended users controller, ", error.message);
-    return res.status(500).json({message:"Internal Server error"})
+    return res.status(500).json({ message: "Internal Server error" });
   }
 };
 
-export const getMyFriends = async (req, res) => {};
+export const getMyFriends = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id)
+      .select("friends")
+      .populate(
+        "friends",
+        "fullName profilePic nativeLanguage learningLanguage"
+      );
+
+    res.status(200).json(user.friends);
+  } catch (error) {
+    console.log("Error in getMyFriends controller", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
