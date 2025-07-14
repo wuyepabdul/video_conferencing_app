44d01a021bc80d4fac90aef43cca21a97d8a7768
wuyepabdul <wuyepabdul@gmail.com>
2025-07-15 00:13:08 +0100

    Add accept friend request controller, and route

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -84,3 +84,29 @@ export const sendFriendRequest = async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const acceptFriendRequest = async (req, res) => {
+  try {
+    const { id: requestId } = req.params;
+    const friendRequest = await FriendRequest.findById(requestId);
+
+    if (!friendRequest) {
+      return res.status(404).json({ message: "Friend request not found" });
+    }
+
+    // verify current user is the recipient
+    if (friendRequest.recipient.toString() !== req.user.id) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to accept this request" });
+    }
+
+    friendRequest.status = "accepted";
+    await friendRequest.save();
+
+    res.status(200).json({ message: "Friend request accepted" });
+  } catch (error) {
+    console.log("Error in accepting friend request", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -15,5 +15,6 @@ router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
 
 router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
 
 export default router;
