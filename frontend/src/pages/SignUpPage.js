b66cfc87319c803c3c4ac661d885e535e3dc9477
wuyepabdul <wuyepabdul@gmail.com>
2025-07-17 06:29:55 +0100

    Design left side of signup page

--- frontend/src/pages/SignUpPage.js
+++ frontend/src/pages/SignUpPage.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { ShipWheelIcon } from "lucide-react";
+import { Link } from "react-router";
 
 const SignUpPage = () => {
   const [signupData, setSignupData] = useState({
@@ -16,7 +18,128 @@ const SignUpPage = () => {
       data-theme="forest"
     >
       <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
-        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col"></div>
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          {/* LOGO */}
+          <div className="mb-4 flex items-center justfy-start gap-2">
+            <ShipWheelIcon className="size-9 text-primary" />
+            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
+              Vid-Conferencing
+            </span>
+          </div>
+
+          <div className="w-full">
+            <form onSubmit={handleSignup}>
+              <div className="space-y-4">
+                <div>
+                  <h2 className="text-xl font-semibold">Create an Account</h2>
+                  <p className="text-sm opacity-70">
+                    Join Vid-conferencing and start your language learning
+                    adventure!
+                  </p>
+                </div>
+
+                <div className="space-y-3">
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text"> Full Name</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="John Doe"
+                      className="input input-bordered w-full"
+                      value={signupData.fullName}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          fullName: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                  </div>
+
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text"> Email</span>
+                    </label>
+                    <input
+                      type="email"
+                      placeholder="johndoe@gmail.com"
+                      className="input input-bordered w-full"
+                      value={signupData.email}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          email: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                  </div>
+
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text"> Password</span>
+                    </label>
+                    <input
+                      type="password"
+                      placeholder="*******"
+                      className="input input-bordered w-full"
+                      value={signupData.password}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          password: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <p className="text-xs opacity-70 mt-1">
+                      Password must be atleast 6 characters long
+                    </p>
+                  </div>
+
+                  <div className="form-control w-full">
+                    <label className="label cursor-pointer justify-start gap-2">
+                      <input
+                        type="checkbox"
+                        className="checkbox checkbox-sm"
+                        required
+                      />
+                      <span className="text-xs leading-tight">
+                        I agree to the
+                        <span className="text-primary hover:underline">
+                          {" "}
+                          Terms of Service
+                        </span>{" "}
+                        and
+                        <span className="text-primary hover:underline">
+                          {" "}
+                          Privacy Policy
+                        </span>
+                      </span>
+                    </label>
+                  </div>
+                </div>
+                <button className="btn btn-primary w-full" type="submit">
+                  {" "}
+                  Create an Account
+                </button>
+                <div className="text-center mt-4">
+                  <p className="text-sm">
+                    Already Have an Account?{" "}
+                    <Link to="/login" className="text-primary hover:underline">
+                      Sign In
+                    </Link>
+                  </p>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* RIGHT SIDE */}
+        
       </div>
     </div>
   );
