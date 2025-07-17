d7422629eb809936e09570c5f18d7d736ca3eb79
wuyepabdul <wuyepabdul@gmail.com>
2025-07-17 05:32:34 +0100

    install lucidee-react package

--- frontend/src/pages/SignUpPage.js
+++ frontend/src/pages/SignUpPage.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SignUpPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
 
-export default SignUpPage
+  const handleSignup = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div
+      className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="forest"
+    >
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col"></div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
