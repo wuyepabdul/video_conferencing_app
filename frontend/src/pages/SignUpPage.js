ed7fbddd9c6c909ccf220abf5296110e60aac350
wuyepabdul <wuyepabdul@gmail.com>
2025-07-17 06:39:26 +0100

    Design right side of signupup page

--- frontend/src/pages/SignUpPage.js
+++ frontend/src/pages/SignUpPage.js
@@ -139,7 +139,28 @@ const SignUpPage = () => {
         </div>
 
         {/* RIGHT SIDE */}
-        
+        <div className="hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center">
+          <div className="max-w-md p-8">
+            {/* illustration */}
+            <div className="relative aspect-square max-w-sm auto">
+              <img
+                src="/frontend/public/signup.png"
+                alt="Language connection illustration"
+                className="w-full h-full"
+              />
+            </div>
+
+            <div className="text-center space-y-3 mt-6">
+              <h2 className="text-xl font-semibold">
+                Connect with language partners worldwide
+              </h2>
+              <p className="opacity-70">
+                Practice conversations, make friends, and improve your language
+                skills together
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
