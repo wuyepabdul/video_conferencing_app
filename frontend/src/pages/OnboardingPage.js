0b4dfb1f9142c25f4eb914c44771fc83749f43f7
wuyepabdul <wuyepabdul@gmail.com>
2025-07-18 06:53:49 +0100

    Design the generate avatar onboarding part

--- frontend/src/pages/OnboardingPage.js
+++ frontend/src/pages/OnboardingPage.js
@@ -3,6 +3,7 @@ import useAuthUser from "../hooks/useAuthUser";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { completOnboarding } from "../lib/api";
+import { CameraIcon, ShuffleIcon } from "lucide-react";
 
 const OnboardingPage = () => {
   const { isLoading, authUser } = useAuthUser();
@@ -30,7 +31,48 @@ const OnboardingPage = () => {
     onboardingMutation(formState);
   };
 
-  return <div>OnboardingPage</div>;
+  const handleRandomAvatar = () => {};
+
+  return (
+    <div className="min-h-screen bg-base-100 flex items-center justify-center p-4">
+      <div className="card bg-base-200 w-full max-w-3xl shadow-xl">
+        <div className="card-body p-6 sm:p-8">
+          <h1 className="text-2xl sm:text-3xl font-bold text-center mb-6">
+            Complete Your Profile
+          </h1>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="flex flex-col items-center justify-center space">
+              <div className="size-32 rounded-full bg-base-300 overflow-hidden">
+                {formState.profilePic ? (
+                  <img
+                    src={formState.profilePic}
+                    alt="Profile Preview"
+                    className="w-full h-full object-cover"
+                  />
+                ) : (
+                  <div className="flex items-center justify-center h-full">
+                    <CameraIcon className="size-12 text-base-content opacity-40" />
+                  </div>
+                )}
+              </div>
+
+              {/* generate random avatar button */}
+              <div className="flex items-center gap-2">
+                <button
+                  type="button"
+                  onClick={handleRandomAvatar}
+                  className="btn btn-accent mt-3"
+                >
+                  <ShuffleIcon   className="size-4 mr-2" />
+                  Generate Random Avatar
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OnboardingPage;
