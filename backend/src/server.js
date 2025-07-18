ae2cb3ab21b43d4d95a589c6e7039bc3de9859d7
wuyepabdul <wuyepabdul@gmail.com>
2025-07-18 20:56:53 +0100

    Add git ignore to base repo

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,10 +5,17 @@ import userRoutes from "./routes/user.routes.js";
 import chatRoutes from "./routes/chat.routes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
