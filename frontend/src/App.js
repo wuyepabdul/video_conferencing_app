3a551c387d4323deecb06e822ae3444673c21c20
wuyepabdul <wuyepabdul@gmail.com>
2025-07-18 20:56:11 +0100

    Redirect user to home if is onboarded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -57,7 +57,15 @@ const App = () => {
         <Route
           path="/onboarding"
           element={
-            isAuthenticated ? <OnboardingPage /> : <Navigate to="/login" />
+            isAuthenticated ? (
+              !isOnboarded ? (
+                <OnboardingPage />
+              ) : (
+                <Navigate to="/" />
+              )
+            ) : (
+              <Navigate to="/login" />
+            )
           }
         />
       </Routes>
