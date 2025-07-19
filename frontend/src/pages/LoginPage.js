0b4268d106232d4a29aa5334c28ca82082a24360
wuyepabdul <wuyepabdul@gmail.com>
2025-07-19 21:19:46 +0100

    Add login logic in login page

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,6 +1,28 @@
-import React from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import React, { useState } from "react";
+import { login } from "../lib/api";
 
 const LoginPage = () => {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const queryClient = useQueryClient();
+  const {
+    mutate: loginMutation,
+    isPending,
+    error,
+  } = useMutation({
+    mutationFn: login,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    loginMutation(loginData);
+  };
+
   return <div>LoginPage</div>;
 };
 
