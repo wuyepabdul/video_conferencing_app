4bbb5e2eaae3d440aad072be90062cc293302951
wuyepabdul <wuyepabdul@gmail.com>
2025-07-20 20:11:36 +0100

    Create logout api function

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -10,6 +10,11 @@ export const login = async (loginData) => {
   return response.data;
 };
 
+export const logout = async () => {
+  const response = await axiosInstance.post("/auth/logout");
+  return response.data;
+};
+
 export const getAuthUser = async () => {
   const res = await axiosInstance.get("/auth/me");
   return res.data;
