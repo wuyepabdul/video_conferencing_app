80589fec66099c32e7fa3d480da483ad8bcb41d3
wuyepabdul <wuyepabdul@gmail.com>
2025-07-23 21:06:09 +0100

    Design FriendCard component

--- frontend/src/components/FriendCard.js
+++ frontend/src/components/FriendCard.js
@@ -1,7 +1,52 @@
 import React from "react";
+import { LANGUAGE_TO_FLAG } from "../constants";
+import { Link } from "react-router";
 
-const FriendCard = () => {
-  return <div>FriendCard</div>;
+const FriendCard = (friend) => {
+  return (
+    <div className="card bg-base-200 hover:shadow-md transition-shadow">
+      <div className="card-body p-4">
+        <div className="flex items-center gap-3 mb-3">
+          <div className="avatar size-12">
+            <img src={friend.profilePic} alt={friend.fullName} />
+          </div>
+          <h3 className="font-semibold truncate">{friend.fullName}</h3>
+        </div>
+
+        <div className="flex flex-wrap gap-1.5 mb-3">
+          <span className="badge badge-secondary text-xs">
+            {getLanguageFlag(friend.nativeLanguage)}
+            Native: {friend.nativeLanguage}
+          </span>
+
+          <span className="badge badge-secondary text-xs">
+            {getLanguageFlag(friend.learningLanguage)}
+            Learning: {friend.learningLanguage}
+          </span>
+        </div>
+        <Link to={`/chat/${friend._id}`} className="btn btn-outline w-full">
+          Message
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default FriendCard;
+
+function getLanguageFlag(language) {
+  if (!language) return null;
+
+  const languageLowercase = language.toLowerCase();
+  const countryCode = LANGUAGE_TO_FLAG[languageLowercase];
+
+  if (countryCode) {
+    return (
+      <img
+        src={`https://flagcdn.com/24x18/${countryCode}`.png}
+        alt={`${languageLowercase} flag`}
+        className="h-3 mr-1 inline-block"
+      />
+    );
+  }
+}
