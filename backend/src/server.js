3d1a053fec49831257713ea69d5936d1d39cdfe1
wuyepabdul <wuyepabdul@gmail.com>
2025-07-25 13:19:23 +0100

    update path to build file

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,11 +27,11 @@ app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
 }
 
 app.listen(PORT, () => {
