f2c636e001a9e8b3cbcfdbbff864623a17a287b7
wuyepabdul <wuyepabdul@gmail.com>
2025-07-25 06:12:45 +0100

    Add deployment scripts in server.js and root folder package.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,12 @@ import chatRoutes from "./routes/chat.routes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 const PORT = process.env.PORT;
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -22,6 +25,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on Port ${PORT}`);
