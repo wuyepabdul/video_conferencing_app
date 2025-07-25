f672e716ba5cc94d886ec21130d24997ac359746
wuyepabdul <wuyepabdul@gmail.com>
2025-07-25 06:17:37 +0100

    update baseurl route to function even in development

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-export const axiosInstance =  axios.create({
-    baseURL:'http://localhost:5001/api',
-    withCredentials:true,
-})
+const BASE_URL =
+  process.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
